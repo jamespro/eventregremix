dd0327e96f6b10ad92e301c7774dca5cbb7bbaf7
James Prickitt <jwprickitt@gmail.com>
2022-09-02 16:28:46 -0400

    formatting updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,8 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    '@remix-run/eslint-config',
+    '@remix-run/eslint-config/node',
     'airbnb',
     'airbnb-typescript',
     'airbnb/hooks',
